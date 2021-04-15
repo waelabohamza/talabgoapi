dd02d32d3479c93dce27f6d6ac1b36fcacc5b86c
waelabohamza <waeleagle1243@gmail.com>
2021-04-16 01:19:22 +0300

    refresh

--- restaurants/items/items.php
+++ restaurants/items/items.php
@@ -6,10 +6,19 @@ $table = "itemsfoodview";
 
 $resid = $_GET['resid'] ; 
 
+if (isset($_GET['catid'])){
+
+   $and = "AND ";         
+
+}else {
+
+   $and = null ;  
+   
+}
+
 $limit = paginationLimit($_GET['page'] ?? null, 10000);
 
-$data = getAllData($table, "categoriesfood_restaurants = '$resid' $limit ");
+$data = getAllData($table, "categoriesfood_restaurants = '$resid' $and $limit ");
 
 createJson($data['count'], $data['values']);
 
-//categoriesfood_restaurants = '$resid' 
