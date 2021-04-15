550d50caa90295151fec67801fc0753d6050e1e4
waelabohamza <waeleagle1243@gmail.com>
2021-04-16 01:48:56 +0300

    refresh

--- restaurants/items/deleteitems.php
+++ restaurants/items/deleteitems.php
@@ -2,24 +2,39 @@
 include "../../connect.php";
 
 $filedir = "items";
+
 $table   = "itemsfood";
 
 if ($_SERVER['REQUEST_METHOD'] == "POST"){
 
     $id         = superFilter($_POST['id']);
+
     $item       = getData($table, "itemsfood_id", $id);
+
     $imageold   = $item['values']['itemsfood_image'];
+
     $checkcat   = $item['count'];
 
     if ($checkcat > 0){
+
         $count = deleteData($table, "itemsfood_id", $id);
+
         if ($count > 0) {
+
             deleteFile($filedir, $imageold);
+
         }
+
         countresault($count);
+
     } else {
+
         failCount();
+
     }
+    
 } else {
+
     failCount();
+
 }
