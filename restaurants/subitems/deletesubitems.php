d0230a54c233af889a4a1dfea4dee4b97e7a73b3
waelabohamza <waeleagle1243@gmail.com>
2021-04-16 16:39:26 +0300

    refresh

--- restaurants/subitems/deletesubitems.php
+++ restaurants/subitems/deletesubitems.php
@@ -10,21 +10,13 @@ if ($_SERVER['REQUEST_METHOD'] == "POST"){
 
     $id         = superFilter($_POST['id']);
 
-    $item       = getData($table, "itemsfood_id", $id);
-
-    $imageold   = $item['values']['itemsfood_image'];
+    $item       = getData($table, "subitemsfood_id", $id);
 
     $checkcat   = $item['count'];
 
     if ($checkcat > 0){
 
         $count = deleteData($table, "itemsfood_id", $id);
-
-        if ($count > 0) {
-
-            deleteFile($filedir, $imageold);
-
-        }
 
         countresault($count);
 
--- restaurants/subitems/editsubitems.php
+++ restaurants/subitems/editsubitems.php
@@ -2,9 +2,8 @@
 
 include "../../connect.php";
 
-$filedir = "items";
 
-$table = "itemsfood";
+$table = "	subitemsfood";
 
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
@@ -12,66 +11,27 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $id             = superFilter($_POST['id']);
     $name           = superFilter($_POST['name']);
     $namear         = superFilter($_POST['namear']);
-    $desc           = superFilter($_POST['desc']);
-    $descar         = superFilter($_POST['descar']);
-    $catid         = superFilter($_POST['catid']);
-    $categoriedata  = getData($table, "itemsfood_id", $id);
-    $count          = $categoriedata['count'];
-    $imageold       = $categoriedata['values']['itemsfood_image'];
+    $price          = superFilter($_POST['price']);
+    $subitemsdata   = getData($table, "subitemsfood_id", $id);
+    $count          = $subitemsdata['count'];
 
 
     // $datauser  =  $user['data'];
     if ($count > 0) {
 
-        if (isset($_FILES['file'])) {
+        $data = array(
+            "subitemsfood_name" => $name,
+            "subitemsfood_name_ar" => $namear,
+            "subitemsfood_price" => $price
+        );
 
-                // Image Request 
+        $where =  "subitemsfood_id = $id ";
 
+        $count =  updateData($table, $data, $where);
 
-                    $image      = image_data("file");
-
-                    $filetmp   =  $image['tmp'];
-
-                    $imagename =  rand(0, 1000000) . "_" . $image['name'];
-
-                    // Request 
-
-            $data = array(
-                "itemsfood_name"        =>  $name,
-                "itemsfood_name_ar"     =>  $namear,
-                "itemsfood_desc"        =>  $desc,
-                "itemsfood_desc_ar"     =>  $descar,
-                "itemsfood_image"       =>  $imagename,
-                "itemsfood_cat"         =>  $catid
-            );
-
-            $where =  "itemsfood_id = $id ";
-
-            $count =  updateData($table, $data, $where);
-
-            deleteFile($filedir, $imageold);
-
-            move_uploaded_file(  $filetmp , "../../upload/" . $filedir . "/" . $imagename);
-      
-        } else {
-
-
-            $data = array(
-                "itemsfood_name"        =>  $name,
-                "itemsfood_name_ar"     =>  $namear,
-                "itemsfood_desc"        =>  $desc,
-                "itemsfood_desc_ar"     =>  $descar,
-                "itemsfood_cat"         =>  $catid
-            );
-
-
-            $where =  "itemsfood_id = $id ";
-
-            $count =  updateData($table, $data, $where);
-        }
+        countresault($count);
     }
-    countresault($count);
 } else {
 
-    zeroCount();
+    failCount();
 }
