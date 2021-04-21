25cb00a8a25036b1387f9ac487fc0d4734dd63ff
waelabohamza <waeleagle1243@gmail.com>
2021-04-21 16:59:16 +0300

    refresh

--- connect.php
+++ connect.php
@@ -5,6 +5,7 @@ $pass = "";
 $option = array(
    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8"
 );
+$countrowinpage = 9 ;  
 try {
    $con = new PDO($dsn, $user, $pass, $option);
    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
--- restaurants/items/searchitems.php
+++ restaurants/items/searchitems.php
@@ -1,18 +1,13 @@
 <?php
-
 include "../../connect.php";
-
 $table = "itemsfoodview";
-
+$search = superFilter($_POST['search']);
 $resid = $_POST['resid'];
 if (isset($_POST['catid']) && $_POST['catid'] == "all") {
     $and = null;
 } else {
     $and   = filterResualt($_POST['catid'] ?? null, "itemsfood_cat");
 }
-
-$limit = paginationLimit($_GET['page'] ?? null, 9);
-
-$data  = getAllData($table, "categoriesfood_restaurants = '$resid' $and $limit ");
-
+$limit = paginationLimit($_GET['page'] ?? null, 100);
+$data  = getAllData($table, "categoriesfood_restaurants = '$resid' And ( itemsfood_name LIKE '%$search%' OR itemsfood_name_ar LIKE '%$search%')  $and $limit ");
 createJson($data['count'], $data['values']);
