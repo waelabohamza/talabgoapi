331cd162cb4f90f44d9c545d5ba15b3c2d6c25f2
waelabohamza <waeleagle1243@gmail.com>
2021-04-22 00:50:34 +0300

    refresh

---
+++ users/itemsfood/itemsfood.php
@@ -0,0 +1,17 @@
+<?php
+
+include "../../connect.php";
+
+$table = "restaurantsview";
+
+$limit = paginationLimit($_GET['page'] ?? null, $countrowinpage);
+
+if (isset($_POST['type']) && $_POST['type'] == "all") {
+    $and = null;
+} else {
+    $and   = filterResualt($_POST['type'] ?? null, "restaurants_type");
+}
+
+$data  = getAllData($table, "1 = 1  $and $limit ");
+
+createJson($data['count'], $data['values']);
