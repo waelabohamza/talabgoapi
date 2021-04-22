bef6123dd2b889fab82d9171960f1b39a850ec2e
waelabohamza <waeleagle1243@gmail.com>
2021-04-22 16:19:19 +0300

    refresh

--- users/itemsfood/itemsfood.php
+++ users/itemsfood/itemsfood.php
@@ -6,7 +6,7 @@ $table = "itemsfoodview";
 
 $limit = paginationLimit($_GET['page'] ?? null, 1000);
 
-$resid  = superFilter($_POSt['resid']);
+$resid  = superFilter($_POST['resid']);
 
 if (isset($_POST['categories']) && $_POST['categories'] == "all") {
     $and = null;
