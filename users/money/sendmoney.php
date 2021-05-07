f9469a04ac0f0323149b198b55a592a462dbc0f0
waelabohamza <waeleagle1243@gmail.com>
2021-05-08 00:21:17 +0300

    r

--- users/money/sendmoney.php
+++ users/money/sendmoney.php
@@ -7,17 +7,17 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $getinfouser = $con->prepare("SELECT * FROM users WHERE users_id = ? LIMIT 1  ");
     $getinfouser->execute(array($userid));
     $datausersender = $getinfouser->fetch(PDO::FETCH_ASSOC);
-    $mybalance = $datausersender['users_balance'];
-    $countone = $getinfouser->rowCount();
+    $mybalance  = $datausersender['users_balance'];
+    $username   = $datausersender['users_name'];
+    $countone   = $getinfouser->rowCount();
     //    التاكد من ان الشخص الذي يريد التحويل موجود   
     if ($countone > 0) {
         //     التاكد من توفر الرصيد الكافي للتحويل 
         if ($mybalance > $price) {
-            $checkuser = $con->prepare("SELECT * FROM users WHERE users_id = '$userid' ");
-            $username = superFilter($_POST['username']);
             $checkuser = $con->prepare("SELECT * FROM users WHERE users_phone = ?");
             $checkuser->execute(array($phone));
             $countuser = $checkuser->rowCount();
+            //     التاكد من ان الشخص الذي تريد التحويل اليه موجود 
             if ($countuser > 0) {
                 $get = getInfoUserByPhone($phone);
                 $useridrecive = $get['users_id'];
@@ -25,9 +25,14 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
                 $count = addMoneyById("users", "users_balance",  $price, "users_phone", $phone);
                 if ($count > 0) {
                     $counttwo = removeMoneyById("users", "users_balance", $price, "users_id", $userid);
-                    echo json_encode(array("status" => "success"));
+                    echo json_encode(array(
+                        "status"  => "success",
+                        "reciver" => $usernamerecive,
+                        "sender"  => $username,
+                        "price"   => $price
+                    ));
                     $title = "TalabGoFoodDelivery";
-                    $message = " تم تحويل رصيد"  . $price . " دينار من قبل  "   . $username  .  " اليك ";
+                    $message = " تم تحويل رصيد "  . $price . " دينار من قبل  "   . $username  .  " اليك ";
                     bill($price, $useridrecive, 1, "تحويل مالي", $message, "users");
                     sendNotifySpecificUser($useridrecive, $title, $message, "", "home");
                     $title = "تنبيه";
