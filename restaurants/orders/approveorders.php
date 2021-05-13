82b0372cbe9728ad105c24ac0963d1024b43c1f1
waelabohamza <waeleagle1243@gmail.com>
2021-05-13 11:52:20 +0300

    refresh

--- restaurants/orders/approveorders.php
+++ restaurants/orders/approveorders.php
@@ -4,10 +4,22 @@ include "../../connect.php";
 
 $table = "ordersfood";
 
+// Request
 $ordersid = superFilter($_POST['ordersid']);
-
+$userid  = superFilter($_POST['userid']);
+$resid  = superFilter($_POST['resid']);
+// ================
 $data = array("ordersfood_status" => 1);
 
 $count = updateData($table, $data, "ordersfood_id = '$ordersid' ");
+
+if ($count > 0) {
+    $title = "هام";
+    $body  =  "تم الطلب بنجاح الرجاء انتظار موافقة المطعم";
+    sendNotifySpecificUser($userid, $title, $body, "", "usersordersfoods1");
+    $title = "هام";
+    $body  =  "يوجد طلب بانتظار الموافقة";
+    sendNotifySpecificRes($resid, $title, $body, "", "resordersfoods1");
+}
 
 countresault($count);
