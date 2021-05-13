04c2b86df798c80143749c2164c761522f64d851
waelabohamza <waeleagle1243@gmail.com>
2021-05-13 11:14:55 +0300

    refresh

--- users/restaurants/orders/deleteorders.php
+++ users/restaurants/orders/deleteorders.php
@@ -1,16 +1,34 @@
-<?php 
+<?php
 
-include "../../../connect.php" ; 
-
-
-$ordersid = superFilter($_POST['ordersid']) ; 
+include "../../../connect.php";
 
 
-$count = deleteData("ordersfood" , "ordersfood_id" , $ordersid , "ordersfood_status = 0") ; 
+$ordersid   = superFilter($_POST['ordersid']);
+$resid      = superFilter($_POST['resid']);
+$resname    = superFilter($_POST['resname']);
+$userid     = superFilter($_POST['userid']);
+$username   = superFilter($_POST['username']);
+$price      = superFilter($_POST['price']);
 
 
-if ($count > 0){
+$count = deleteData("ordersfood", "ordersfood_id", $ordersid, "AND ordersfood_status = 0 ");
 
-   
 
+if ($count > 0) {
+
+    $counttwo      = removeMoneyById("restaurants", "restaurants_balance", $price, "restaurants_id", $resid);
+
+    $countthree    = addMoneyById("users", "users_balance", $price, "users_id", $userid);
+
+    // For User
+    if ($counttwo > 0 && $countthree > 0){
+        $title = "الغاء طلب طعام";
+        $body  = " الغاء طلب طعام من المطعم " . $resname;
+        $countStageFivePartOne =    bill($price, $userid, 1, $title, $body, "users");
+        $body  = " الغاء طلب طعام من الزبون " . $username;
+        $countStageFivePartTwo =    bill($price, $resid, 1, $title, $body, "restaurants");
+    }
+    successCount();
+} else {
+    failCount();
 }
