69fc64faee1a4ddaabca9d2a9176d9c6a8dd8d29
waelabohamza <waeleagle1243@gmail.com>
2021-05-23 13:30:12 +0300

    refresh

---
+++ restaurants/orders/closetable.php
@@ -0,0 +1,26 @@
+<?php
+
+include "../../connect.php";
+
+$table = "ordersfood";
+
+// Request
+$ordersid = superFilter($_POST['ordersid']);
+$userid  = superFilter($_POST['userid']);
+$resid  = superFilter($_POST['resid']);
+
+// ================
+$data = array("ordersfood_status" => 3);
+
+$count = updateData($table, $data, "ordersfood_id = '$ordersid' ");
+
+if ($count > 0) {
+    $title = "هام";
+    $body  =  "شكرا لزيارتك مطعمنا نتمنى ان تكون الوجبة نالت على اعجابكم ";
+    sendNotifySpecificUser($userid, $title, $body, "", "ordersfoodscreenptwo");
+    $title = "هام";
+    $body  =  "تم ااغلاف  الطاولة  بنجاح";
+    sendNotifySpecificRes($resid, $title, $body, "", "Rordersfoodscreentwo");
+}
+
+countresault($count);
