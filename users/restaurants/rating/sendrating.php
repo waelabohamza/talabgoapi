2756020504d3740e7588e6d03c18b6cd2713e100
waelabohamza <waeleagle1243@gmail.com>
2021-06-03 19:21:33 +0300

    refresh

--- users/restaurants/rating/sendrating.php
+++ users/restaurants/rating/sendrating.php
@@ -8,9 +8,17 @@ $userid = superFilter($_POST['userid']) ;
 $resid = superFilter($_POST['resid']) ; 
 $ordersid = superFilter($_POST['ordersid']) ; 
 $data = array(
-    ""
+    "rating_value" => $rating , 
+    "rating_comment" => $comment , 
+    "rating_userid" => $userid , 
+    "rating_sid" => $resid , 
+    "rating_orders" => $ordersid , 
+    "rating_type" => "restaurants"
 );
 
+$count = insertData("rating" , $data); 
+
+countresault($count) ; 
 
 
 ?>
