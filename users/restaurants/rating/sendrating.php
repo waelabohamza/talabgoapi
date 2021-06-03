8916e082f9430d851f9385589b9c2f8c4b45b796
waelabohamza <waeleagle1243@gmail.com>
2021-06-03 19:19:15 +0300

    refresh

--- users/restaurants/rating/sendrating.php
+++ users/restaurants/rating/sendrating.php
@@ -6,6 +6,7 @@ $rating = superFilter($_POST['rating']) ;
 $comment = superFilter($_POST['comments']) ; 
 $userid = superFilter($_POST['userid']) ; 
 $resid = superFilter($_POST['resid']) ; 
+$ordersid = superFilter($_POST['ordersid']) ; 
 $data = array(
     ""
 );
