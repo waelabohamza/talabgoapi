a88be7084c2225b6c4c17daa836e76025349c41f
waelabohamza <waeleagle1243@gmail.com>
2021-06-12 19:53:56 +0300

    refresh

--- admin/taxi/approve.php
+++ admin/taxi/approve.php
@@ -1,22 +1,26 @@
 <?php
 
-include "../../connect.php";  
-  
+include "../../connect.php";
+
 $table = "taxi";
 
 // Request ==================
 
 $id  = superFilter($_POST['id']);
 
-$minprice = superFilter($_POST['minprice']) ; 
+$minprice = superFilter($_POST['minprice']);
 
-$price = superFilter($_POST['price']) ; 
- 
-// ================
+$price = superFilter($_POST['price']);
 
-$data = array("taxi_approve" => 1);
+$data = array(
+    "taxi_mincharge" => $minprice,
+    "taxi_price"     => $price,
+    "taxi_approve"   => 1
+);
 
-$count = updateData($table, $data, "taxi_id = '$id'");
-
+$count  = updateData("taxi", $data, "taxi_id = $id ");
 
 countresault($count) ; 
+ 
+ 
+// ================
--- taxi/auth/signin.php
+++ taxi/auth/signin.php
@@ -5,13 +5,13 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
         $token    = $_POST['token'];
         $email    = superFilter($_POST['email']);
         $password = sha1($_POST['password']);
-        $data     = signInWithEmailAndPassword("users", "users_email", "users_password", $email, $password, "AND users_approve = 1");
+        $data     = signInWithEmailAndPassword("taxi", "taxi_email", "taxi_password", $email, $password, "AND taxi_approve = 1");
         $users    = $data['values'];
         $count    = $data['count'];
         if ($count > 0) {
-            $count2 = insertToken($users['users_id'], $token, "users");
+            $count2 = insertToken($users['taxi_id'], $token, "taxi");
 
-            sendNotifySpecificUser($users['users_id'], "مرحيا", "مرحبا بك في تطبيق    TalabGo", "", "");
+            sendNotifySpecificUser($users['taxi_id'], "مرحيا", "مرحبا بك في تطبيق التكسي الشامل", "", "");
 
             echo json_encode(array("status" => "success", "users" => $users, "token" => $token));
         } else {
